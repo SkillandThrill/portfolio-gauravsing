1563efcb10d3907e7d493186b2450d616d8aa5b2
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-03 19:21:46 +0530

    adding another aceternity ui component Bento grid for a header component.. in that bento grid to add the values created the data folder and file to export the info so that there is less code redundancy for the assets adding the updated public folder with images needed for the website..

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
         </h2>
         <TextGenerateEffect
           className='text-center text-[40px] md:text-5xl lg:text-6xl'
-          words='Transforming Concepts into Seamless Experiences'
+          words='Transforming Concepts into Seamless User Experiences'
         />
 
         <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'> 
