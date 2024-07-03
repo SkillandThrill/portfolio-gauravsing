aa1cdfb573c28a37f9e19dcdc3262bc06292f9d9
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-03 10:35:30 +0530

    To achieve the dark theme for the dot grid we installed the node next themes. created a theme provider file in app: added the TextGemeratorEffect ui from Aceternity for the animation..last for the show my work we are adding the Aceternity button with the animation.. updated the globals css and with that the hero section is done!

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight';
+import { TextGenerateEffect } from './ui/TextGenerateEffect';
+import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa6';
 
 const Hero = () => {
   return (
@@ -8,9 +11,32 @@ const Hero = () => {
       <Spotlight className='top-10 left-full h-[80vh] w-[50vh]' fill='purple'/>
       <Spotlight className='top-28 left-80 h-[80vh] w-[50vh]' fill='blue'/>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
-    
+      </div>
+
+    <div className=' flex justify-center relative my-20 z-10'>
+      <div className='max-w-[89vh] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+          Dynamic Web Magic with Next.js
+        </h2>
+        <TextGenerateEffect
+          className='text-center text-[40px] md:text-5xl lg:text-6xl'
+          words='Transforming Concepts into Seamless Experiences'
+        />
+
+        <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'> 
+          Hi, I&apos;m Gaurav. A Next.js Developer based in Pune.
+        </p>
+        <a href="#about"> 
+        <MagicButton
+          title='Show my Work'
+          icon ={<FaLocationArrow/>}
+          position='right'
+        />
+        </a>
+        
+      </div>
     </div>
 
     </div>
