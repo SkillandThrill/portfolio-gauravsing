8aa4fc08712f6092df93ce079e366b34688a4549
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-04 13:03:27 +0530

    Customizing the last grid of the bento grid for that installing a animation package named as lottie to copy the email and initiate the copying functionality created a state and a handleClick on Magic Button with this completed the header section and the bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from '@/data/confetti.json'
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -9,6 +16,8 @@ export const BentoGrid = ({
   className?: string;
   children?: React.ReactNode;
 }) => {
+  
+
   return (
     <div
       className={cn(
@@ -42,10 +51,16 @@ export const BentoGridItem = ({
   titleClassName ?:string
   spareImg ?:string
 }) => {
+  const [copied,setCopied] = useState(false)
+  const handleCopy = () =>{
+    navigator.clipboard.writeText('contact.gauravsingrajput@gmail.com');
+
+    setCopied(true);
+  }
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       style={{
@@ -53,7 +68,7 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)'
       }}
     >
-      <div className={`${id===6} && 'flex justify-center h-full'`}>
+      <div className={`${id===6 && 'flex justify-center'} h-full`}>
           <div className="w-full h-full absolute">
             {img &&(
               <img
@@ -74,7 +89,7 @@ export const BentoGridItem = ({
           </div>
           {id ===6 &&(
             <BackgroundGradientAnimation>
-              <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+              {/* <div className="absolute z-50 flex items-center justify-center text-white font-bold"/> */}
             </BackgroundGradientAnimation>
           )}
 
@@ -111,6 +126,28 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
+
+        {id===6 && (
+          <div className="mt-5 relative">
+            <div className={`absolute -bottom-5 right-0`}>
+              <Lottie options={{
+                loop: copied,
+                autoplay: copied,
+                animationData,
+                rendererSettings:{
+                  preserveAspectRatio: 'xMidYMid slice',
+                }
+              }}/>
+            </div>
+            <MagicButton
+              title={copied ? 'Email copied' : 'Copy my email'}
+              icon={<IoCopyOutline/>}
+              position="left"
+              otherClasses="`bg-[#161a31]`"
+              handleClick={handleCopy}
+            />
+          </div>
+        )}
         </div>
       </div>
     </div>
