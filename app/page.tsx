688dded021d7be1d304f370c747a28a513f35021
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-05 22:15:55 +0530

    made some adjustment in the bento grid to create a good display of the data..

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,14 @@ import { FloatingNav } from '../components/ui/FloatingNav';
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import RecentProjects from '../components/RecentProjects';
+import { navItems } from '@/data';
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[{
-            name:'Home',link:'/',icon:<FaHome/>
-          }]}
+          navItems={navItems}
         />
         <Hero/>
         <Grid/>
