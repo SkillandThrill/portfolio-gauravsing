903bd58d04b9595691e76afbdfea48ac2d41cfa4
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-05 22:00:28 +0530

    displaying the RecentProjects with the acerternity ui component as 3d pin fot that imported the ui component into the codebase.. adding the data into the 3d pin container while adding classes..with that we are done with the recent projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import Hero from '../components/Hero';
 import { FloatingNav } from '../components/ui/FloatingNav';
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProjects from '../components/RecentProjects';
 
 export default function Home() {
   return (
@@ -15,6 +15,7 @@ export default function Home() {
         />
         <Hero/>
         <Grid/>
+        <RecentProjects/>
       </div>
     </main>
   );
